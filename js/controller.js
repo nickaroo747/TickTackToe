9b2eb8a11e43ca6980eaf4252e6b1e722f94bd46
Nicholas Mitchell <mitchell.nicholas@yahoo.com>
2015-04-01 19:30:27 -0700

    player turn logic

--- js/controller.js
+++ js/controller.js
@@ -2,88 +2,106 @@ angular
     .module("tictactoe")
     .controller("tictactoe", tictactoe);
 
-    function tictactoe(){
-        var self = this;
+tictactoe.$inject = ['$firebaseObject', '$firebaseArray']; //injected my controller with firebase
 
+function tictactoe($firebaseObject, $firebaseArray){
+    var self = this;
+    self.scoretest1 = 0;
+    self.scoretest2 = 0;
+    self.pickSquare = pickSquare;
 
-self.listholes = [
-        {
-            cellNumber: "one"
-        },
-        {
-            cellNumber: "two"
-        },
-        {
-            cellNumber: "three"
-        },
-        {
-            cellNumber: "four"
-        },
-        {
-            cellNumber: "five"
-        },
-        {
-            cellNumber: "six"
-        },
-        {
-            cellNumber: "seven"
-        },
-        {
-            cellNumber: "eight"
-        },
-        {
-            cellNumber: "nine"
-        },
-        
+    var pepsiScore = 0; // established pepsi's score is 0
+    var cokeScore = 0; // established coke's score is 0
 
-    ] //end of kitties array
-    }; // end of Kitties controller function
-    
+    var coke = ("images/cokesm.png")
+    var pepsi = ("images/pepsism.png")
+    var blank = ("images/coke.png")
 
-// angular
-//     .module("todoApp")
-//     .controller("TodosController", TodosController);
+    var playerturn = 0;
 
+self.listholes = [ 
+        { name: "1", image: "images/blank.png"},
+        { name: "2", image: "images/blank.png"},
+        { name: "3", image: "images/blank.png"},
+        { name: "4", image: "images/blank.png"},
+        { name: "5", image: "images/blank.png"},
+        { name: "6", image: "images/blank.png"},
+        { name: "7", image: "images/blank.png"},
+        { name: "8", image: "images/blank.png"},
+        { name: "9", image: "images/blank.png"},
+    ] 
 
-// TodosController.$inject = ['$firebaseArray', '$firebaseObject'];
+      function pickSquare($index){
+        var ref = new Firebase('https://cokevspepsi.firebaseio.com');
+        var playerSelection = $firebaseObject(ref);
+          
+          if (playerturn === 1 || 3 || 5 || 7){
+            self.listholes[$index].image = pepsi;
+            self.scoretest2++;
+            playerturn++;
+            // return playerturn;
+          } else 
 
+          if (playerturn === 0 || 4 || 6 || 8){
+            self.listholes[$index].image = coke;
+            self.scoretest1++;
+            playerturn++;
+            // return playerturn;
+          }
 
-//     function TodosController($firebaseArray, $firebaseObject){
-//         var self = this;
+          console.log(playerturn);
 
-//         self.text; //bound to input box for new todo
-//         self.addTodo = addTodo;
-//         self.deleteTodo = deleteTodo;
-//         self.completedTodos = completedTodos;
-//         self.remainingTodos = remainingTodos;
-//         self.search; //bound to input box
-//         self.todoList = getToDosList();
-
-//         function addTodo(){
-//             self.todoList.$add({task: self.text, done: false});
-//             self.text = null;
-//         }
-
-//         function getToDosList(){
-//             var ref = new Firebase('https://nickstodolist.firebaseio.com/todos');
-//             var todos = $firebaseArray(ref);
-
-//             console.log(todos);
-//             return todos;
-//         } 
-
-
-//         function deleteTodo(todo){
-//             self.todoList.$remove(todo);
-//         }
-
-//         function completedTodos(){ var count = 0; for(var i = 0; i < self.todoList.length; i++){ if(self.todoList[i].done){ count++; } } return count; }
-
-//         function remainingTodos(){ var count = 0; for(var i = 0; i < self.todoList.length; i++){ if(self.todoList[i].done === false){ count++; } } return count; }
+        }
+    }; // End of the function GuacamoleController($firebaseObject)
 
 
 
-//     }
+
+
+
+
+
+
+
+
+
+
+
+/* command + option + / */ 
+
+//IF THEN STATEMENTS//
+// if cell numbers 1,2,3 are equal / winner
+// if cell numbers 4,5,6 are equal / winner
+// if cell numbers 7,8,9 are equal / winner
+
+// if cell numbers 1,4,7 are equal / winner
+// if cell numbers 2,5,8 are equal / winner
+// if cell numbers 3,6,9 are equal / winner
+
+// if cell numbers 1,5,9 are equal / winner
+// if cell numbers 3,5,7 are equal / winner
+
+
+//DOn't Forget to Post the Score
+        // function postScore(){
+        //     var ref = new Firebase('https://guacamolenrm.firebaseio.com/pScore');
+        //     var pScore = $firebaseObject(ref);
+
+        //     pScore.$loaded(function(){
+        //         pScore.holes = [];
+        //         pScore.currentMolePosition = 0;
+        //         pScore.score = 0;
+        //         for(var i = 0; i <9; i++){
+        //           pScore.holes.push({moleIsHere: false});
+        //         }
+                    
+        //         pScore.$save();
+        //         console.log(pScore);
+        //         whackMole(0);
+        //     })
+        //     return pScore;
+        // } 
+
 
 
 
